b52caeb410b4e6877f05d4ad744df0640110fef7
aassou <aassou.abdelilah@gmail.com>
2024-05-07 00:51:51 +0100

    Add OpenApi API documentation

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,15 +2,18 @@ package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends PagingAndSortingRepository<Car, Long>, CrudRepository<Car, Long> {
-    List<Car> findByBrand(String brand);
+    List<Car> findByBrand(@Param("brand") String brand);
 
-    List<Car> findByModel(String model);
+    List<Car> findByModel(@Param("model") String model);
 
-    List<Car> findByColor(String color);
+    List<Car> findByColor(@Param("color") String color);
 
-    List<Car> findByModelYear(int modelYear);
+    List<Car> findByModelYear(@Param("modelYear") int modelYear);
 }
